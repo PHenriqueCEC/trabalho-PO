90ea252de1b3ad606b203f1610b1f94157424690
Matheus Gomes Luz Werneck <matheus.werneck@pampa.compasso>
2022-08-20 22:04:37 -0300

    Criado serviço para para o LinearSolver

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
-import express from 'express';
-import optimizeRoutes from './routes/optimize.route';
+import express from "express";
+import optimizeRoutes from "./routes/optimize.route";
+import Solver from "./services/LinearSolver";
 
 const app = express();
-const port = process.env.PORT || '2030';
+const port = process.env.PORT || "2030";
 
+const solver = new Solver();
 
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+const res = solver.execute();
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
 });
 
-app.use('/optimize', optimizeRoutes);
+app.use("/optimize", optimizeRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at https://localhost:${port}`);
 });
-
---
+++ src/services/LinearSolver.ts
@@ -0,0 +1,57 @@
+// @ts-nocheck
+import GLPK, { Options, Result } from "glpk.js";
+export default class Solver {
+  glpk: any;
+  options: Options;
+
+  constructor() {
+    this.glpk = GLPK();
+    this.options = {
+      msglev: this.glpk.GLP_MSG_ALL,
+      presol: true,
+      cb: {
+        call: (progress: String) => console.log(progress),
+        each: 1,
+      },
+    };
+  }
+
+  execute(): Result {
+    const res = this.glpk.solve(
+      {
+        name: "LP",
+        objective: {
+          direction: this.glpk.GLP_MAX,
+          name: "obj",
+          vars: [
+            { name: "x1", coef: 30 },
+            { name: "x2", coef: 20 },
+          ],
+        },
+        subjectTo: [
+          {
+            name: "cons1",
+            vars: [
+              { name: "x1", coef: 2 },
+              { name: "x2", coef: 1 },
+            ],
+            bnds: { type: this.glpk.GLP_UP, ub: 8, lb: 0.0 },
+          },
+          {
+            name: "cons2",
+            vars: [
+              { name: "x1", coef: 1 },
+              { name: "x2", coef: 3 },
+            ],
+            bnds: { type: this.glpk.GLP_UP, ub: 8.0, lb: 0.0 },
+          },
+        ],
+      },
+      this.options
+    );
+
+    console.log(res);
+
+    return res;
+  }
+}
